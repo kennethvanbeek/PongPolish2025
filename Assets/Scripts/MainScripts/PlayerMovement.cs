f84608cad235730b4d82ce326d6db1c4dab19ddf
ROC-Kenneth <87307361+kennethvanbeek@users.noreply.github.com>
2025-07-04 11:58:01 +0200

    initial commit

---
+++ Assets/Scripts/MainScripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 20f;
+    [SerializeField] float yClampValue = 7.5f;
+    [SerializeField] string playerInputName;
+
+    void Update()
+    {
+        if (string.IsNullOrEmpty(playerInputName)) return; // checkt of inputnaam bestaat
+
+        float input = Input.GetAxis(playerInputName);
+        float newY = Mathf.Clamp(transform.position.y + (input * Time.deltaTime * playerSpeed), -yClampValue, yClampValue);
+        transform.position = new Vector3(transform.position.x, newY, 0);
+    }
+
+    public string GetPlayerInputName() => playerInputName;
+}
